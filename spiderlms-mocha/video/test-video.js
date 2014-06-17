a43c8cb6acb876217e793402765646116ffe3757
Jonathan Pollock <jpollock@mobiquityinc.com>
2014-06-17 00:07:50 -0400

    added Test3

--- spiderlms-mocha/video/test-video.js
+++ spiderlms-mocha/video/test-video.js
@@ -69,4 +69,36 @@ describe('Test Video', function(){
     		};
     	});
 	});
+
+	it('Test3: Retrieve a specific Video - GET request', function(done) {
+	  TheAPI
+	  	.patch('/videos/2')
+	  	.send()
+	  	.expect(200)
+        .end(function(err, res) {
+        	if(err){
+        		log.error('ERROR: ' +err);
+        	}
+        	else {
+        		log.info('Test2: Response: '+JSON.stringify(res.body));
+            	log.info('Test2: Response code: '+res.statusCode);
+            	assert.equal(res.statusCode, 200);
+            	assert.equal(res.body[0].id, 2);
+            	assert.equal(res.body[0].title, 'Javascript Callbacks');
+            	assert.equal(res.body[0].description, 'This is a description for the Spidey callbacks video. yay.');
+            	assert.equal(res.body[0].url, 'https://SpiderLMS.s3.amazonaws.com/23234234LSKDJFSDF');
+            	assert.equal(res.body[0].uploaded_by, 2);
+            	assert.equal(res.body[0].created_at, '2014-03-17 10:10:10');
+            	assert.equal(res.body[0].updated_at, '2014-03-17 10:10:10');
+            	assert.eaual(res.body[0].tags, 'tag1, tag2, tag3, tag4');
+            	assert.equal(res.body[0].assets[0].id, 1);
+            	assert.equal(res.body[0].assets[0].title, 'The slide show');
+            	assert.equal(res.body[0].assets[0].filetype, 'pptx');
+            	assert.equal(res.body[0].assets[0].fileurl, 'http:this.is.an.amazon.s3.url');
+
+            	done();
+            };
+        });
+	});
+
 });
